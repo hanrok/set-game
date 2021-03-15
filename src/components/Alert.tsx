a88abca1ee603f2c1627abf6a0c66eba4e3fb9f7
Javier Diaz <coderdiaz@gmail.com>
2021-03-15 13:28:26 -0600

    feat: added alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,20 @@
+import classNames from "classnames";
+
+interface IAlertProps {
+  type?: 'info' | 'success' | 'error' | 'warning' | string;
+  message?: string | React.ReactNode;
+}
+
+const Alert = ({ type = "info", message }: IAlertProps) => {
+  const className = classNames("text-center p-3 rounded mb-6 font-semibold", {
+    "bg-gray-600 text-gray-200": (type === 'info'),
+    "bg-green-300 text-green-900": (type === 'success'),
+    "bg-yellow-200 text-yellow-900": (type === 'warning'),
+    "bg-red-300 text-red-800": (type === 'error'),
+  })
+  return <div className={className}>
+    {message}
+  </div>;
+}
+
+export default Alert;
