8cf01ec9eb957c4de4ad52577aaed76cf943199d
Javier Diaz <coderdiaz@gmail.com>
2021-03-18 17:09:11 -0600

    feat: added logic to show a modal when not sets available

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import { useContext } from "react";
 import Header from "@/components/partials/Header";
+import Modal from "@/components/Modal";
+import { Context, ContextValues } from "./Context";
 
 interface ILayoutProps {
   children?: React.ReactNode;
 }
 
 const Layout = ({ children }: ILayoutProps) => {
+  const { nsets, addThreeCards } = useContext(Context) as ContextValues;
+
+  const handleEventModal = () => addThreeCards();
+
   return <>
     <Header />
     <main className="py-4">
       {children}
     </main>
+    { nsets?.length === 0 && <Modal onClick={handleEventModal} /> }
   </>
 };
 
