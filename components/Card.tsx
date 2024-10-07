b7dadd7af895e8bc2dabab2678b943d878aabb24
Hanan <hananrok@gmail.com>
2024-10-07 09:44:22 +0300

    change the category to the name of the product in the card view

--- components/Card.tsx
+++ components/Card.tsx
@@ -37,7 +37,7 @@ const Card = ({ config, onClick }: ICardProps) => {
           <div className="flex-grow w-full h-full bg-center bg-contain bg-no-repeat" style={{ backgroundImage: `url('${config.logo}')` }}>
           </div>
           <div className="text-sm pt-2 font-medium">
-            {config.category}
+            {config.name}
           </div>
         </div>
       </button>
